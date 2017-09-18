253c02ce37dfbbd07f9430880e6f8978e672d297
shockingjmh <shockingjmh@naver.com>
2017-09-18 16:06:52 +0900

    commit

---
+++ Wearable_WiFi.ino
@@ -0,0 +1,84 @@
+#include <avr/pgmspace.h>
+
+#define WIFI_ENABLE_PIN 13
+#define DEBUG true
+
+#define SSID "Project11x"      //your wifi ssid here
+#define PASS "abcd1234"   //your wifi wep key here
+
+void setup()
+{
+  Serial1.begin(9600);
+  Serial1.setTimeout(10000);
+  Serial.begin(9600);  //can't be faster than 19200 for softserial
+
+  if (DEBUG) {
+    while (!Serial);
+  }
+
+  pinMode(WIFI_ENABLE_PIN, OUTPUT);
+  digitalWrite(WIFI_ENABLE_PIN, HIGH);
+  sendData("AT\r\n", 5000);//Test AT startup
+  //sendData("AT+GMR\r\n", 5000);//View version info
+  sendData("AT+CWMODE=3\r\n", 5000); //WiFi mode
+  //sendData("AT+CWLAP\r\n", 5000);//List available APs
+  sendData("AT+CWJAP=\"Project11x\",\"abcd1234\"\r\n", 5000); //join AP
+  sendData("AT+CIPMUX=0\r\n", 5000);
+  sendData("AT+CIFSR\r\n", 5000); //get ip address
+  sendData("AT+CIPSTART=\"TCP\",\"192.168.0.12\",10023\r\n", 10000);
+  sendData("AT+CIPMODE=1\r\n", 5000);
+  sendData("AT+CIPSEND=0,5\r\n", 6000);
+  /*Serial1.print("AT+RST\r\n");
+    Serial1.print("AT+CWMODE=3\r\n");
+    Serial1.print("AT+CWJAP=\"Project11x\",\"abcd1234\"\r\n");
+    Serial1.print("AT+CIPMUX=0\r\n");
+    Serial1.print("AT+CIFSR\r\n");
+    Serial1.print("AT+CIPSTART=\"TCP\",\"192.168.0.7\",9001\r\n");
+    Serial1.print("AT+CIPMODE=1\r\n");
+    Serial1.print("AT+CIPSEND=0,5\r\n");
+    Serial.println("Start Socket Client");*/
+}
+
+void loop() {
+
+  /*delay(5000);
+    clearRx();
+    delay(1000);*/
+    
+  /*if (Serial1.find("+IPD.")){
+    delay(1000);
+    int connectionID = Serial1.read()-48;
+    Serial.println((char)connectionID);
+    }*/
+  recvData();
+  for(int i = 0; i < 10; i++){
+     sendData("a\r\n", 2000); 
+    }
+}
+
+void recvData() {
+  while (Serial1.available() > 0) {
+    Serial.println(Serial1.read());
+  }
+}
+
+
+String sendData(String command, const int timeout)
+{
+  String response = "";
+
+  Serial1.print(command); // send the read character to the esp8266
+  long int time = millis();
+  while ( (time + timeout) > millis()) {
+    while (Serial1.available()) {
+      // The esp has data so display its output to the serial window
+      char c = (uint8_t)Serial1.read(); // read the next character.
+      response += c;
+    }
+  }
+  if (DEBUG)  {
+    Serial.print(response);
+    Serial.print("\n");
+  }
+  return response;
+}
